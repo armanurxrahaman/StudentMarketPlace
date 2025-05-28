596b7b427f36295ea8b61a4f3bb3dab6ac7354bd
armanurxrahaman <ayanurrahaman0266@gmail.com>
2025-05-28 20:56:09 +0530

    Update FIrebase config to use environment variables

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,16 +1,17 @@
 const admin = require("firebase-admin");
-const path = require("path");
-const fs = require("fs");
 
-const serviceAccountPath = path.join(__dirname, "../serviceAccountKey.json");
-
-console.log("Looking for Firebase key at:", serviceAccountPath);
-
-if (!fs.existsSync(serviceAccountPath)) {
-  throw new Error(`❌ serviceAccountKey.json not found at: ${serviceAccountPath}`);
-}
-
-const serviceAccount = require(serviceAccountPath);
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: "108548604822800603599",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${process.env.FIREBASE_CLIENT_EMAIL}`,
+  universe_domain: "googleapis.com"
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
